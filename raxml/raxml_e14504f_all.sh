35f3f70be2082c517c6753c09488caf57bbdd647
dspak <daniel.spakowicz@gmail.com>
2017-05-25 00:04:25 -0400

    lsu and rpb2 tree

---
+++ raxml/raxml_e14504f_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --ntasks=10 --nodes=1
+#SBATCH --time=12:00:00
+#SBATCH --job-name=raxml_e14504f
+#SBATCH --mail-user=daniel.spakowicz@yale.edu
+#SBATCH --mail-type=ALL
+
+# Produced by DS_tree.Rmd
+# Dan Spakowicz
+# 2017-05-24 21:35:01
+# Submission file for the RAxML analysis of the E14504F tree using the sequencing in the google sheet 'E14504F'
+
+cd /project/fas/gerstein/djs88/e14504f/bayes
+module load Apps/MrBayes/3.2.2
+mpirun mb all.nex
